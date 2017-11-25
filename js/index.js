83b5eb8d5f86215edfdf1898fa6a4432c627c52d
Aubrey Henderson <amhenderson@sei.cmu.edu>
2017-11-25 14:31:33 -0500

    added modal

--- js/index.js
+++ js/index.js
@@ -7,6 +7,30 @@
     }
     
     $(document).ready(function(){
+        
+        $("#modal").animatedModal({
+            modalTarget: 'animatedModal',
+            animatedIn: 'slideInRight',
+            animatedOut: 'slideOutRight',
+            color: '#20252a' /* black */
+            /*,
+                // Callbacks
+            beforeOpen: function() {
+                console.log("The animation was called");
+            },           
+            afterOpen: function() {
+                console.log("The animation is completed");
+            }, 
+            beforeClose: function() {
+                console.log("The animation was called");
+            }, 
+            afterClose: function() {
+            console.log("The animation is completed");
+            }
+            */
+        });
+
+        
         $("#show_more").click(function(){
             $(".mobile_hidden").show();
             $("#show_more").hide();
