258c83220100dbfc57a41a9c9885d6c476a5917e
Aubrey Henderson <amhenderson@sei.cmu.edu>
2017-11-25 12:26:53 -0500

    resized images

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-    $('.grid').masonry({
-      // options
-      itemSelector: '.grid-item',
-      columnWidth: 220
-    });
+    function hover(){
+        $("#palette").attr("src", "assets/icons/palette2.png");
+    }
 
+    function unhover(){
+        $("#palette").attr("src", "assets/icons/palette.png");
+    }
+    
+    $(document).ready(function(){
+        $("#show_more").click(function(){
+            $(".mobile_hidden").show();
+            $("#show_more").hide();
+        });
+    });
