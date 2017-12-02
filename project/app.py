36cf84c1f3b7e82c967c00a0b35b57b99f5eefa1
Aubrey Henderson <amhenderson@sei.cmu.edu>
2017-12-02 14:56:55 -0500

    fixed runtime error

--- project/app.py
+++ project/app.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 
 REPO_NAME = "SSUI-Homework-6"  # Used for FREEZER_BASE_URL
